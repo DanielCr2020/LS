8bd98047f19f15e1bb8d1638d17fd171aaed0518
DanielCr2020 <danielclass2020@gmail.com>
2024-05-24 17:49:19 -0400

    (Mostly) done implementation of ls -l
    Updated Makefile

--- Makefile
+++ Makefile
@@ -1,4 +1,10 @@
-all:
-	gcc ls.c -o ls -Wall -g
+TARGET=ls
+CC=gcc
+FLAGS=-Wall -Wpedantic -pedantic-errors -g -fstack-protector-all -O2
+
+all: $(TARGET)
+
+ls: ls.c
+	$(CC) ls.c -o ls $(FLAGS)
 clean:
 	rm ls
--- ls.c
+++ ls.c
@@ -7,10 +7,13 @@
 #include <sys/stat.h>
 #include <stddef.h>
 #include <pwd.h>
+#include <grp.h>
 #include <sys/types.h>
 #include <linux/limits.h>
 #include <stdbool.h>
 #include <errno.h>
+#include <sys/ioctl.h>
+#include <time.h>
 
 #define BLUE "\x1b[34;1m"
 #define DEFAULT "\x1b[0m"
@@ -57,10 +60,19 @@ void getFlagsAndDirs(int argc, char** const inputArgs, char* outputFlags, char**
 
 //these will be populated with information from ls(), and then sorted and printed
 
+//Contains information about each file in a directory
 typedef struct itemInDir {
     char* name;     //name of the file itself
     char* path;     //path to file - used for stat()
-    bool isDir;
+    bool isDir;     //Used for colorization
+
+    char* fileType; //?
+    char* permissions;  //permissions of the file
+    int hardLinksCount;
+    char* owner;
+    char* group;
+    size_t size;    //file size
+    long mtime;    //modified time
 } itemInDir;        //each item in a directory
 
 typedef struct folderInfo {
@@ -70,6 +82,7 @@ typedef struct folderInfo {
     itemInDir* items;   //array of item info structures for that directory
     int itemCount;      //number of items in directory
     bool doWePrint;     //do we print the contents of this folder?
+    size_t totalSize;   //for -l, shows sum of size of all the items in the directory
 } folderInfo;           //one folder read by ls
 
 /**
@@ -80,11 +93,50 @@ typedef struct folderInfo {
 void whichDirs(char** const inputDirs, char** outputDirs, int arg){
 
 }
+/**
+ * @brief Get information used in long list format printing
+ * @param item: A pointer to the item information struct. Modified by function.
+ */
+void getLongListItems(itemInDir* item){
+    char permissions[]="----------";
+    if(item->isDir==true){
+        permissions[0]='d';
+    }
+    struct stat fileStat;
+    if(stat(item->path,&fileStat)<0){
+        fprintf(stderr,"Error: stat() failed. %s\n",strerror(errno));
+    }
+    strcpy(&permissions[1], (fileStat.st_mode & S_IRUSR) ? "r" : "-");
+    strcpy(&permissions[2], (fileStat.st_mode & S_IWUSR) ? "w" : "-");
+    strcpy(&permissions[3], (fileStat.st_mode & S_IXUSR) ? "x" : "-");
+    strcpy(&permissions[4], (fileStat.st_mode & S_IRGRP) ? "r" : "-");
+    strcpy(&permissions[5], (fileStat.st_mode & S_IWGRP) ? "w" : "-");
+    strcpy(&permissions[6], (fileStat.st_mode & S_IXGRP) ? "x" : "-");
+    strcpy(&permissions[7], (fileStat.st_mode & S_IROTH) ? "r" : "-");
+    strcpy(&permissions[8], (fileStat.st_mode & S_IWOTH) ? "w" : "-");
+    strcpy(&permissions[9], (fileStat.st_mode & S_IXOTH) ? "x" : "-");
+
+    item->permissions=malloc(sizeof(permissions));
+    memcpy(item->permissions,permissions,sizeof(permissions));
+    item->hardLinksCount=fileStat.st_nlink;
+
+    struct passwd* pwd;
+    struct group* grp;
+
+    pwd=getpwuid(fileStat.st_uid);
+    grp=getgrgid(fileStat.st_gid);
+
+    item->owner=pwd->pw_name;
+    item->group=grp->gr_name;
+    item->size=fileStat.st_size;
+    item->mtime=fileStat.st_mtime;
+    // printf("PERMISSIONS    %s\n",item->permissions);
+}
 
 /**
  * @brief In a given directory, which items do we need to run ls on
  * Also gives us the path to the items to make stat() easier
- * Returns number of items to print. Accounts for -a and -A flags
+ * Returns number of items to print. Accounts for -a and -A flags. Also checks if an item is a directory.
  * @param dir The current directory we are searching through 
  * @param flags Flags from argv. If 'a' or 'A' are in the flags, for example, that will affect the outputItems
  * @param outputItems The items in that folder that will be listed when the dir contents are printed
@@ -101,6 +153,7 @@ int whichItems(char* const dir, char* const flags, itemInDir* outputItems){
     int index=0;
     while((dirp=readdir(dp))!=NULL){
         //strchr(flags,'a')==NULL   -> 'a' is not a given flag
+        //these cause valgrind errors. Will fix later
         char* hasa=strchr(flags,'a');
         char* hasA=strchr(flags,'A');
         if(!hasa && !hasA){
@@ -114,7 +167,7 @@ int whichItems(char* const dir, char* const flags, itemInDir* outputItems){
                 continue;
             }
         }
-        char itemPath[PATH_MAX];
+        char itemPath[PATH_MAX+1];      //+1 for newline
         strncpy(itemPath,dir,PATH_MAX);
         size_t dirnameLen=strnlen(dir,PATH_MAX-1);
         if(itemPath[dirnameLen-1]!='/'){
@@ -124,12 +177,27 @@ int whichItems(char* const dir, char* const flags, itemInDir* outputItems){
         strncat(itemPath,dirp->d_name,PATH_MAX);
         (outputItems[index]).name=strndup(dirp->d_name,256);
         (outputItems[index]).path=strndup(itemPath,PATH_MAX);
+    
+        struct stat fileStat;
+        if(stat(outputItems[index].path,&fileStat)==-1){
+            fprintf(stderr,"Error: stat(%s) failed: %s\n",outputItems[index].path,strerror(errno));
+        }
+        if(S_ISDIR(fileStat.st_mode)==1){
+            outputItems[index].isDir=true;
+        }
+        else{
+            outputItems[index].isDir=false;
+        }
+
+        char* hasl=strchr(flags,'l');
+        if(hasl){
+            getLongListItems(&outputItems[index]);
+        }
         index++;
     }
     closedir(dp);
     return index;
 }
-
 
 /**
  * @brief The ls logic itself. Populates the structs above with information about folders and files in those 
@@ -183,23 +251,18 @@ void ls(char* const flags, size_t argDirCount, size_t* printDirCount, char** con
         //allocate space for info for each item in dir
         folders[i].items=(itemInDir*)malloc(totalItemsInDir*sizeof(itemInDir));
         folders[i].itemCount=whichItems(dirs[i],flags,folders[i].items);
-        //See which items are actually directories (colorization)
-        for(int j=0;j<folders[i].itemCount;j++){
-            struct stat fileStat;
-            //stat needs an absolute path
-            if(stat(folders[i].items[j].path, &fileStat)==-1) {
-                fprintf(stderr,"Error: stat(%s) failed: %s\n",folders[i].items->path,strerror(errno));
-            }
-            if(S_ISDIR(fileStat.st_mode)==1){
-                folders[i].items[j].isDir=true;
-            }
-            else{
-                folders[i].items[j].isDir=false;
-            }
-        }
+
         folders[i].doWePrint=true;
     }
 
+}
+
+char* trimTime(char* timeString, char* outputString){
+    outputString=calloc(13,sizeof(char));
+    for(int i=4;i<16;i++){
+        outputString[i-4]=timeString[i];
+    }
+    return outputString;
 }
 
 /**
@@ -238,23 +301,65 @@ void printLS(size_t argDirCount, size_t printDirCount, folderInfo* folders, char
             startIndex=printableFolders[i].itemCount-1;
             step=-1;
         }
+        char* hasl=strchr(flags,'l');
         //print each item in each printable folder, colorzing directories as blue
-        for(int j=startIndex;step==-1 ? j>=0 : j<printableFolders[i].itemCount;j+=step){
-            if(printableFolders[i].items[j].isDir==true){
-                printf("%s%s%s   ",BLUE, printableFolders[i].items[j].name, DEFAULT);
+        if(hasl){
+            for(int j=startIndex;step==-1 ? j>=0 : j<printableFolders[i].itemCount;j+=step){
+                char* timeString;
+                timeString=trimTime(ctime(&printableFolders[i].items[j].mtime),timeString);
+                // printf("Time string 2: %s ",timeString);
+                printf("%s %d %s %s %ld %s %s%s%s",
+                    printableFolders[i].items[j].permissions,
+                    printableFolders[i].items[j].hardLinksCount,
+                    printableFolders[i].items[j].owner,
+                    printableFolders[i].items[j].group,
+                    printableFolders[i].items[j].size,
+                    timeString,
+                    printableFolders[i].items[j].isDir==true ? 
+                        BLUE : DEFAULT,
+                    printableFolders[i].items[j].name,DEFAULT
+
+                );
+                //colorize durectories as blue
+                if(printableFolders[i].items[j].isDir==true){
+                    // printf(" %s%s%s",BLUE,printableFolders[i].items[j].name,DEFAULT);
+                }
+                else if(printableFolders[i].items[j].isDir==false){
+                    // printf(" %s",printableFolders[i].items[j].name);
+                }
+                
+                if(step==-1 && j>0){
+                    printf("\n");
+                }
+                else if(step==1 && j<printableFolders[i].itemCount-1){
+                    printf("\n");
+                }
+
+                //cleanup
+                free(printableFolders[i].items[j].name);
+                free(printableFolders[i].items[j].path);
+                free(printableFolders[i].items[j].permissions);
             }
-            else if(printableFolders[i].items[j].isDir==false){
-                printf("%s   ",printableFolders[i].items[j].name);
+        }
+        else {
+            for(int j=startIndex;step==-1 ? j>=0 : j<printableFolders[i].itemCount;j+=step){
+                if(printableFolders[i].items[j].isDir==true){
+                    printf("%s%s%s  ",BLUE, printableFolders[i].items[j].name, DEFAULT);
+                }
+                else if(printableFolders[i].items[j].isDir==false){
+                    printf("%s  ",printableFolders[i].items[j].name);
+                }
+                free(printableFolders[i].items[j].name);
+                free(printableFolders[i].items[j].path);
             }
-            free(printableFolders[i].items[j].name);
-            free(printableFolders[i].items[j].path);
         }
         //don't print a blank line for folders with no items
         if(printableFolders[i].itemCount>0){
             printf("\n");
         }
     }
-    
+
+    //Cleanup
     for(int i=0;i<argDirCount;i++){
         if(folders[i].hasHeader){
             free(folders[i].header);
@@ -273,8 +378,6 @@ int main(int argc, char* argv[]){
     memcpy(args,argv,argc*sizeof(char*));
     // for(int i=1;i<argc;i++){
         // printf("%s\n",args[i]);
-    // }
-
     char flags[1024];
     char** directories=malloc(argc*sizeof(char*));
 
